7da344cc0c0f5d5fd52fba1ad8725c49a06678b6
Zeeshan <Zeeshan.tlntrip@gmail.com>
2023-06-12 18:13:46 +0600

    Note CRUD API Developed

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Note::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required',
+            'user_id' => 'required'
+        ]);
+        return Note::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Note::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $note = Note::find($id);
+        $note->update($request->all());
+
+        return $note;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Note::destroy($id);
+    }
+
+
+    /**
+     * Search for the note title
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($key)
+    {
+        return Note::where('title', 'like', '%' . $key . '%')->get();
+    }
+}
